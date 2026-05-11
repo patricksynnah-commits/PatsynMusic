59d19528f7abfae1a432139566f0ae94e2a063ca
Patrick <patricksynnah@gmail.com>
2026-05-11 11:43:20 +0000

    Removed incompatible audio packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:on_audio_query/on_audio_query.dart';
-import 'package:just_audio/just_audio.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() {
