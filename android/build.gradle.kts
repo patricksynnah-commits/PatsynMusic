469c708cf3a37e7defb3180be884bef0e907679d
Patrick <patricksynnah@gmail.com>
2026-05-11 11:09:51 +0000

    Fixed Gradle afterEvaluate issue

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,29 +5,19 @@ allprojects {
     }
 }
 
-val newBuildDir: Directory =
+val newBuildDir =
     rootProject.layout.buildDirectory
         .dir("../../build")
         .get()
+
 rootProject.layout.buildDirectory.value(newBuildDir)
 
 subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
+    project.layout.buildDirectory.value(
+        newBuildDir.dir(project.name)
+    )
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-subprojects {
-    afterEvaluate {
-        extensions.findByName("android")?.let {
-            if (it is com.android.build.gradle.LibraryExtension) {
-                it.namespace = "com.example.temp"
-            }
-        }
-    }
 }
